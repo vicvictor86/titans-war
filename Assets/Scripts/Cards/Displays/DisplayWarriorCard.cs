f54a0a2404cc430babc4b070a592bf380f63eb7a
Victor <vicbem@hotmail.com>
2024-03-11 20:05:03 -0300

    implemented: hover card animation

--- Assets/Scripts/Cards/Displays/DisplayWarriorCard.cs
+++ Assets/Scripts/Cards/Displays/DisplayWarriorCard.cs
@@ -2,7 +2,6 @@ using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class DisplayWarriorCard : DisplayCard<WarriorCard>, IPointerEnterHandler, IPointerExitHandler
 {
@@ -14,8 +13,6 @@ public class DisplayWarriorCard : DisplayCard<WarriorCard>, IPointerEnterHandler
     public TextMeshProUGUI PlainsForceText;
 
     private Animator anim;
-    private Transform parentToReturnTo = null;
-    private int siblingIndex;
     private bool isOverable = true;
 
     protected override void Start()
@@ -28,36 +25,29 @@ public class DisplayWarriorCard : DisplayCard<WarriorCard>, IPointerEnterHandler
         MountainsForceText.text = Card.MountainsForce.ToString();
         PlainsForceText.text = Card.PlainsForce.ToString();
         anim = gameObject.GetComponent<Animator>();
-        parentToReturnTo = transform.parent;
     }
-
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (!isOverable) {
             return;
         }
-        Debug.Log("entrou");
-        siblingIndex = gameObject.transform.GetSiblingIndex();
+
         var siblings = gameObject.transform.parent.GetComponentsInChildren<DisplayWarriorCard>();
         foreach (var sibling in siblings)
         {
             sibling.isOverable = false;
         }
         anim.SetBool("IsMouseOver", true);
-        gameObject.transform.SetParent(gameObject.transform.parent.transform.parent, true);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        Debug.Log("saiu");
-        anim.SetBool("IsMouseOver", false);
         var siblings = gameObject.transform.parent.GetComponentsInChildren<DisplayWarriorCard>();
         foreach (var sibling in siblings)
         {
             sibling.isOverable = true;
         }
-        gameObject.transform.SetParent(parentToReturnTo, true);
-        gameObject.transform.SetSiblingIndex(siblingIndex);
+        anim.SetBool("IsMouseOver", false);
     }
 }
